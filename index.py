6124d33f562fc8b2a8f27528a0a44e295c833b4f
mifan <luckyNoder@163.com>
2018-07-26 22:33:39 +0800

    update

--- index.py
+++ index.py
@@ -1,3 +1,15 @@
-print(r'hello,\n world')  #转换转义字符
+class Student ():
+    name  = 'mifan'
+    def __init__ (self , name , age):
+        name = name 
+        ge = age
+  
 
-"hello,wrold"[1:0]
+
+student = Student('小米',12)
+
+print(student.name)
+print(Student.name)
+print(student.__dict__)
+print(Student.__dict__)
+print(Student.__doc__)
